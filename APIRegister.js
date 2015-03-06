6b27225d26e7634440b37604478fb4edcb454666
di-ruan <ruandi88@gmail.com>
2015-03-05 23:57:30 -0500

    implement GET API

--- APIRegister.js
+++ APIRegister.js
@@ -4,10 +4,28 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/ecomm_database');
+mongoose.connect('mongodb://localhost/userinfo');
+
+var schema = new mongoose.Schema({
+	username: String,
+	password: String,
+	key: String
+});
+
+var model = mongoose.model('user', schema);
 
 app.get('/getkey', function(req, res) {
-	res.send('registration is done');
+	var username = req.query.username;
+	var password = req.query.password;
+	if(username == undefined || password == undefined) {
+		res.send('need username and password');	
+	} else {
+		var key = username + '_' + password;
+		var record = new model({username: username, password: password, key: record	});
+		console.log(record);
+		record.save();
+		res.send(key);
+	}
 });
 
 app.listen(4242);
