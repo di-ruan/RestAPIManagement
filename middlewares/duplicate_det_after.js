481c18db6dfb493044e9dfc248ceb5b20ee31e87
Qiming Chen <simoncqm@gmail.com>
2015-03-18 05:27:03 -0400

    add duplicate detection, but code not that robust

--- middlewares/duplicate_det_after.js
+++ middlewares/duplicate_det_after.js
@@ -1,2 +1,13 @@
+var express = require("express");
+var http = require("http");
+var bodyParser = require('body-parser');
+var app = express();
 
+app.use(bodyParser.json());
 
+app.post("*", function(request, response){
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(request.body));
+});
+
+http.createServer(app).listen(9706);
